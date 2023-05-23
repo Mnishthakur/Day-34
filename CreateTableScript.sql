a395eb23ff298d8e221ae0e93c585ede264bb57f
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-05-23 15:19:06 +0530

    Create CreateTableScript.sql

---
+++ CreateTableScript.sql
@@ -0,0 +1,19 @@
+use MyDatabase;
+
+IF (NOT EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo'
+                 AND  TABLE_NAME = 'EMPLOYEE'))
+BEGIN
+	CREATE TABLE EMPLOYEE
+	(
+	Id INT IDENTITY PRIMARY KEY,
+	Name VARCHAR(50) NOT NULL,
+	Salary DECIMAL NOT NULL,
+	Designation VARCHAR(40) NOT NULL,
+	DeptId INT NOT NULL
+	)
+	-- SEED DATA
+	INSERT INTO EMPLOYEE VALUES('SampleUser',50000, 'HR', 20);
+	select * from EMPLOYEE;
+END
